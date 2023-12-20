ffe8a97c941f7eac598533bb9c70d9b2cce58d29
Julian Camacho <jjulian.341@gmail.com>
2023-12-19 22:11:12 -0600

    Added about me section animation with stars

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,12 +8,13 @@ export default function AboutMe() {
 
     const { scrollYProgress } = useScroll({
         target: ref,
-        offset: ["start start", "end center"],
+        offset: ["start start", "end start"],
     });
 
     const yHeader = useTransform(scrollYProgress, [0, 1], ["40%", "-120%"]);
     const yText = useTransform(scrollYProgress, [0, 1], ["30%", "-50%"]);
     const yImg = useTransform(scrollYProgress, [0, 1], ["50%", "-200%"]);
+    const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
     return (
         <section id="AboutMe" className="about--section" ref={ref}>
@@ -27,11 +28,14 @@ export default function AboutMe() {
             <div className="hero--section--content--box about--section--box">
                 <div className="hero--section--content">
                     <p className="sub--title">About</p>
-                    <motion.h1 style={{ y: yHeader }}  className="skills--section--heading">About Me</motion.h1>
-                    <motion.p style={{ y: yText }} p className="hero--section--description">I'm a Computer Engineering student at Costa Rica Institute of Technology. Through my career a have worked in several projects that involved learning multiple programming languages, algorithms and data structures, frameworks, database engines, HDLs, operating systems and other technologies and tools.</motion.p>
-                    <motion.p style={{ y: yText }} p className="hero--section--description">I love playing soccer, working out at the gym, playing video games, and hanging out with my friends. Additionally, I absolutely love pasta!</motion.p>
+                    <motion.h1 style={{ y: yHeader }} className="skills--section--heading">About Me</motion.h1>
+                    <motion.p style={{ y: yText }} className="hero--section--description">I'm a Computer Engineering student at Costa Rica Institute of Technology. Through my career a have worked in several projects that involved learning multiple programming languages, algorithms and data structures, frameworks, database engines, HDLs, operating systems and other technologies and tools.</motion.p>
+                    <motion.p style={{ y: yText }} className="hero--section--description">I love playing soccer, working out at the gym, playing video games, and hanging out with my friends. Additionally, I absolutely love pasta!</motion.p>
                 </div>
             </div>
+
+            <motion.div style={{ y: yBg }} className="planets"> </motion.div>
+            <motion.div style={{ x: yBg }} className="stars"> </motion.div>
         </section>
     )
 }
